1b63c181c5d85801ca9c796c227d10dc617bc03b
Felix <felix.lapierre1@gmail.com>
2020-02-21 17:21:47 -0500

    Observer

--- src/server/FileServer.java
+++ src/server/FileServer.java
@@ -1,7 +1,15 @@
 package server;
 
+import server.library.HttpRequest;
 import server.library.HttpRequestObserver;
+import server.library.HttpResponse;
 
 class FileServer implements HttpRequestObserver {
+
+    @Override
+    public HttpResponse onRequest(HttpRequest request) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
