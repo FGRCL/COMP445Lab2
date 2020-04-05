f4ff1d9f13075830470a7a6833bc0e3dfef05be7
FGRCL <35940434+FGRCL@users.noreply.github.com>
2020-04-05 14:12:11 -0400

    Add locale to the router

--- src/router/Router.java
+++ src/router/Router.java
@@ -1,12 +1,13 @@
 package router;
 
 import java.io.IOException;
+import java.util.Locale;
 
 public class Router{
 	private static Process routerProcess;
 	public static void start(int port, float dropRate, String maxDelay, int seed) {
 		try {
-            String command = String.format("router_x64.exe --port=%d --drop-rate=%.2f --max-delay=%s --seed=%d", port, dropRate, maxDelay, seed);
+            String command = String.format(Locale.CANADA, "router_x64.exe --port=%d --drop-rate=%.2f --max-delay=%s --seed=%d", port, dropRate, maxDelay, seed);
             routerProcess = Runtime.getRuntime().exec(command);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
